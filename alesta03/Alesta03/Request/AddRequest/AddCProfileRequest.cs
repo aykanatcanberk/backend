b897a7308c7e52dc96cf9754cec19cd5161d1246
Merve Yasar <merve.yasar.ce@gmail.com>
2023-08-02 20:32:09 +0300

    düzeltme

--- alesta03/Alesta03/Request/AddRequest/AddCProfileRequest.cs
+++ alesta03/Alesta03/Request/AddRequest/AddCProfileRequest.cs
@@ -7,7 +7,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string FDate { get; set; }
-        public int TotalStaff { get; set; }
+        public string TotalStaff { get; set; }
         public string Location { get; set; }
         public string Prof { get; set; }
         public string Phone { get; set; }
